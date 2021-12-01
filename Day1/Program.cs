ad845d0a832937c33fe7f1f7675a54a479d9a141
Xwilarg <xwilarg@yahoo.fr>
2021-12-01 17:30:48 +0100

    Add part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -3,12 +3,31 @@ using System.IO;
 using System.Linq;
 
 var data = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToArray();
-int count = 0;
-for (int i = 0; i < data.Length - 1; i++)
+
+#region part 1
 {
-    if (data[i] < data[i + 1])
+    int count = 0;
+    for (int i = 0; i < data.Length - 1; i++)
     {
-        count++;
+        if (data[i] < data[i + 1])
+        {
+            count++;
+        }
     }
+    Console.WriteLine(count);
 }
-Console.WriteLine(count);
+#endregion
+
+#region part 2
+{
+    int count = 0;
+    for (int i = 0; i < data.Length - 3; i++)
+    {
+        if (data[i..(i + 3)].Sum() < data[(i + 1)..(i + 4)].Sum())
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+#endregion
