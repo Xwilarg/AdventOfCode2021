07de059c43f45e9912a05eb3983c6fac9011cc24
Xwilarg <xwilarg@yahoo.fr>
2021-12-01 14:05:10 +0100

    Add day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var data = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < data.Length - 1; i++)
+{
+    if (data[i] < data[i + 1])
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
