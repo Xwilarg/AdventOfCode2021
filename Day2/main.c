1b1790dd9941d0f9de3ce1ed9d7e2edbcb59d428
Xwilarg <xwilarg@yahoo.fr>
2021-12-02 15:52:32 +0100

    Add day 2 part 2

--- Day2/main.c
+++ Day2/main.c
@@ -1,42 +1,64 @@
 #include <stdio.h>
 
-int main()
+static int horizontal;
+static int depth;
+
+// For part 2
+static int aim;
+
+static void part1(int dir, int value)
 {
-    int horizontal = 0;
-    int depth = 0;
+    if (dir == 'f') {
+        horizontal += value;
+    } else if (dir == 'd') {
+        depth += value;
+    } else {
+        depth -= value;
+    }
+}
+
+static void part2(int dir, int value)
+{
+    if (dir == 'd') {
+        aim += value;
+    } else if (dir == 'u') {
+        aim -= value;
+    } else {
+        horizontal += value;
+        depth += value * aim;
+    }
+}
+
+static void exercice(void (*treatment)(int, int))
+{
+    horizontal = 0;
+    depth = 0;
+    aim = 0;
 
     FILE* file = fopen("input.txt", "r");
     char instruction[1];
     int dir;
     while (fread(instruction, 1, 1, file) > 0)
     {
+        dir = instruction[0];
         switch (instruction[0])
         {
             case 'f':
                 fseek(file, 7, SEEK_CUR); // "orward "
-                dir = 0;
                 break;
             
             case 'd':
                 fseek(file, 4, SEEK_CUR); // "own "
-                dir = 1;
                 break;
             
             case 'u':
                 fseek(file, 2, SEEK_CUR); // "p "
-                dir = 2;
                 break;
         }
 
         fread(instruction, 1, 1, file);
         int value = instruction[0] - '0';
-        if (dir == 0) {
-            horizontal += value;
-        } else if (dir == 1) {
-            depth += value;
-        } else {
-            depth -= value;
-        }
+        treatment(dir, value);
 
         // If we are on Windows, endlines are \r\n while on Linux it's just \n
         fread(instruction, 1, 1, file);
@@ -47,5 +69,11 @@ int main()
     }
     printf("%d\n", horizontal * depth);
     fclose(file);
+}
+
+int main(void)
+{
+    exercice(part1);
+    exercice(part2);
     return 0;
 }
