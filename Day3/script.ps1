eacfc1f42ca53cc458079ddbbe6c48c0a7af6d28
Xwilarg <xwilarg@yahoo.fr>
2021-12-03 11:18:31 +0100

    Add beginning of day 3 part 2

--- Day3/script.ps1
+++ Day3/script.ps1
@@ -1,29 +1,71 @@
 $Content = Get-Item -Path .\input.txt | Get-Content
 $Length = $Content[0].Length
 
-$Gamma = 0
-$Epsilon = 0
+function Part1 {
+    $Gamma = 0
+    $Epsilon = 0
 
-$Bit = 1
+    $Bit = 1
 
-($Length - 1)..0 | ForEach-Object {
-    $i = $_
-    $Zero = 0
-    $One = 0
-    $Content | ForEach-Object {
-        if ($_[$i] -eq '0') {
-            $Zero++
+    ($Length - 1)..0 | ForEach-Object {
+        $i = $_
+        $Zero = 0
+        $One = 0
+        $Content | ForEach-Object {
+            if ($_[$i] -eq '0') {
+                $Zero++
+            } else {
+                $One++
+            }
+        }
+
+        if ($Zero -gt $One) {
+            $Gamma += $Bit
         } else {
-            $One++
+            $Epsilon += $Bit
+        }
+
+        $Bit += $Bit
+    }
+    Write-Output ($Gamma * $Epsilon)
+}
+
+function Part2 {
+    $Oxygen = New-Object System.Collections.Generic.List[System.String]
+    $CO2 = New-Object System.Collections.Generic.List[System.String]
+
+    $Content | ForEach-Object {
+        $Oxygen.Add($_)
+        $CO2.Add($_)
+    }
+
+    ($Length - 1)..0 | ForEach-Object {
+        $i = $_
+        $Zero = 0
+        $One = 0
+        $Oxygen | ForEach-Object {
+            if ($_[$i] -eq '0') {
+                $Zero++
+            } else {
+                $One++
+            }
+        }
+        $Target = ($Zero -gt $One) ? '0' : '1'
+        ($Oxygen.Count - 1)..0 | ForEach-Object {
+            if ($Oxygen[$_][$i] -ne $Target) {
+                $Oxygen.RemoveAt($_)
+            }
         }
     }
-
-    if ($Zero -gt $One) {
-        $Gamma += $Bit
-    } else {
-        $Epsilon += $Bit
+    $Bit = 1
+    $Value = 0
+    $Oxygen[0].ToCharArray() | ForEach-Object {
+        $Value += $Bit * ($_ - '0')
+        $Bit += $Bit
     }
+    echo $Value
 
-    $Bit += $Bit
 }
-Write-Output ($Gamma * $Epsilon)
+
+Part1
+Part2
