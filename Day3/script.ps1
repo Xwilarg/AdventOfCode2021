5fc267d063129b419b537bdd59b6dbcfd2682692
Xwilarg <xwilarg@yahoo.fr>
2021-12-03 11:45:19 +0100

    Add day 3 part 2

--- Day3/script.ps1
+++ Day3/script.ps1
@@ -38,34 +38,45 @@ function Part2 {
         $Oxygen.Add($_)
         $CO2.Add($_)
     }
-
-    0..($Length - 1) | ForEach-Object {
-        $i = $_
-        $Zero = 0
-        $One = 0
-        $Oxygen | ForEach-Object {
-            if ($_[$i] -eq '0') {
-                $Zero++
+    function Parse($List, $UseZero) {
+        0..($Length - 1) | ForEach-Object {
+            if ($List.Count -eq 1) {
+                return
+            }
+            $i = $_
+            $Zero = 0
+            $One = 0
+            $List | ForEach-Object {
+                if ($_[$i] -eq '0') {
+                    $Zero++
+                } else {
+                    $One++
+                }
+            }
+            if ($UseZero) {
+                $Target = ($Zero -gt $One) ? '0' : '1'
             } else {
-                $One++
+                $Target = ($Zero -gt $One) ? '1' : '0'
+            }
+            ($List.Count - 1)..0 | ForEach-Object {
+                if ($List[$_][$i] -ne $Target) {
+                    $List.RemoveAt($_)
+                }
             }
         }
-        $Target = ($Zero -gt $One) ? '0' : '1'
-        ($Oxygen.Count - 1)..0 | ForEach-Object {
-            if ($Oxygen[$_][$i] -ne $Target) {
-                $Oxygen.RemoveAt($_)
-            }
+        $Bit = 1
+        $Value = 0
+        $Nb = $List[0].ToCharArray()
+        ($Nb.Length - 1)..0 | ForEach-Object {
+            $Value += ($Bit * ($Nb[$_] - 48))
+            $Bit += $Bit
         }
+        return $Value
     }
-    $Bit = 1
-    $Value = 0
-    $Nb = $Oxygen[0].ToCharArray()
-    ($Nb.Length - 1)..0 | ForEach-Object {
-        $Value += ($Bit * ($Nb[$_] - 48))
-        $Bit += $Bit
-    }
-    echo $Value
 
+    $V1 = Parse $Oxygen $True
+    $V2 = Parse $CO2 $False
+    Write-Output ($V1 * $V2)
 }
 
 Part1
