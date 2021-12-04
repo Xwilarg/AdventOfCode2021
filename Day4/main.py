ee45730d93668b9200bc22320110057f5e5acb0d
Xwilarg <xwilarg@yahoo.fr>
2021-12-04 19:51:48 +0100

    Begin day 4

---
+++ Day4/main.py
@@ -0,0 +1,36 @@
+lines = open("input.txt", "r").read().split('\n')
+numbers = lines[0].split(',')
+
+grids = []
+
+rowSize = 5
+for i in range(2, len(lines), rowSize + 1):
+    current = []
+    for y in range(0, rowSize):
+        current.append(list(filter(None, lines[i].split(' '))))
+        data = []
+        for x in range(0, rowSize):
+            data.append(list(filter(None, lines[i + x].split(' ')))[y])
+        current.append(data)
+    grids.append(current)
+
+isDone = False
+score = 0
+for i in range(0, len(numbers)):
+    for g in grids:
+        for arr in g:
+            if numbers[i] in arr:
+                arr.remove(numbers[i])
+            if len(arr) == 0:
+                for row in g:
+                    for elem in row:
+                        if elem not in numbers[:(i + 1)]:
+                            score += int(elem)
+                score *= int(numbers[i])
+                isDone = True
+                break
+        if isDone:
+            break
+    if isDone:
+        break
+print(score)
