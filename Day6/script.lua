31b34991f7cadfd91e483a3aec83d83cee560910
Xwilarg <xwilarg@yahoo.fr>
2021-12-06 16:39:09 +0100

    Add day 6 part 2

--- Day6/script.lua
+++ Day6/script.lua
@@ -1,31 +1,39 @@
 local f = io.open("input.txt")
 
-lifetime = {}
--- lua arrays starts at 1 so we need to do + 1 everywhere
-newBorn = 8 + 1
-reset = 6 + 1
-for i = 1, newBorn do
-    table.insert(lifetime, 0)
-end
+fileData = f:read()
 
-for nb in string.gmatch(f:read(), "([^,]+)") do
-    lifetime[tonumber(nb) + 1] = lifetime[tonumber(nb) + 1] + 1
-end
-
-for year = 1, 80 do
-    nextBorns = lifetime[1]
-    for i = 1, newBorn - 1 do
-        lifetime[i] = lifetime[i + 1]
+function lanternfish(dayCount)
+    lifetime = {}
+    -- lua arrays starts at 1 so we need to do + 1 everywhere
+    newBorn = 8 + 1
+    reset = 6 + 1
+    for i = 1, newBorn do
+        table.insert(lifetime, 0)
     end
-    lifetime[reset] = lifetime[reset] + nextBorns
-    lifetime[newBorn] = nextBorns
+
+    for nb in string.gmatch(fileData, "([^,]+)") do
+        lifetime[tonumber(nb) + 1] = lifetime[tonumber(nb) + 1] + 1
+    end
+
+    for year = 1, dayCount do
+        nextBorns = lifetime[1]
+        for i = 1, newBorn - 1 do
+            lifetime[i] = lifetime[i + 1]
+        end
+        lifetime[reset] = lifetime[reset] + nextBorns
+        lifetime[newBorn] = nextBorns
+    end
+
+    sum = 0
+    for i = 1, newBorn do
+        sum = sum + lifetime[i]
+    end
+
+    print(sum)
+
 end
 
-sum = 0
-for i = 1, newBorn do
-    sum = sum + lifetime[i]
-end
-
-print(sum)
+lanternfish(80)
+lanternfish(256)
 
 f:close()
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Advent Of Code 2021
  - Day 2: [C (gcc)](Day2)
  - Day 3: [Powershell](Day3)
  - Day 4: [Python](Day4)
- - Day 5: [Javascript (Node)](Day4)
+ - Day 5: [Javascript (Node)](Day5)
+ - Day 6: [Lua](Day6)
