7dd7efee6b5a641b2e8a95fabf83236eb655d39e
Xwilarg <xwilarg@yahoo.fr>
2021-12-06 16:32:13 +0100

    Add day 6 part 1

---
+++ Day6/script.lua
@@ -0,0 +1,31 @@
+local f = io.open("input.txt")
+
+lifetime = {}
+-- lua arrays starts at 1 so we need to do + 1 everywhere
+newBorn = 8 + 1
+reset = 6 + 1
+for i = 1, newBorn do
+    table.insert(lifetime, 0)
+end
+
+for nb in string.gmatch(f:read(), "([^,]+)") do
+    lifetime[tonumber(nb) + 1] = lifetime[tonumber(nb) + 1] + 1
+end
+
+for year = 1, 80 do
+    nextBorns = lifetime[1]
+    for i = 1, newBorn - 1 do
+        lifetime[i] = lifetime[i + 1]
+    end
+    lifetime[reset] = lifetime[reset] + nextBorns
+    lifetime[newBorn] = nextBorns
+end
+
+sum = 0
+for i = 1, newBorn do
+    sum = sum + lifetime[i]
+end
+
+print(sum)
+
+f:close()
