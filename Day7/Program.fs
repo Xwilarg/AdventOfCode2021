d7e8adbec790a8690aba996ec926a3447b74a75a
Xwilarg <xwilarg@yahoo.fr>
2021-12-07 11:02:22 +0100

    Add day 7 part 2

--- Day7/Program.fs
+++ Day7/Program.fs
@@ -2,14 +2,18 @@
 open System.IO
 open System.Linq
 
-[<EntryPoint>]
-let main _ =
+let calculateDistance f =
     let data =
         File.ReadAllText("input.txt").Split [|','|]
         |> Seq.map(fun x -> x |> int)
     let value =
         [0..(data.Max() + 1)]
-        |> Seq.map (fun x -> data.Sum(fun d -> Math.Abs(d - x)))
+        |> Seq.map (fun x -> data.Sum(fun d -> (int)(f(Math.Abs(d - x)))))
         |> Seq.min
-    printf "%d" value
+    printf "%d\n" value
+
+[<EntryPoint>]
+let main _ =
+    calculateDistance (fun x -> x)
+    calculateDistance (fun x -> [1..x] |> Seq.sum)
     0
