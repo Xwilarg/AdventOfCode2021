84e0e5b548d136bab5cd4c2fbff1ed2a9f0c9e87
Xwilarg <xwilarg@yahoo.fr>
2021-12-08 22:01:41 +0100

    Split code into functions

--- Day8/main.rs
+++ Day8/main.rs
@@ -1,11 +1,10 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use io::BufReader;
 
-
-fn main() {
-    let file = File::open("input.txt").unwrap();
+fn part1(lines: std::io::Lines<BufReader<File>>) {
     let mut count = 0;
-    for line in io::BufReader::new(file).lines() {
+    for line in lines {
         for data in line.unwrap().split('|').nth(1).unwrap().trim().split(' ') {
             if data.len() == 2 || data.len() == 3 || data.len() == 4 || data.len() == 7 {
                 count += 1;
@@ -13,4 +12,20 @@ fn main() {
         }
     }
     println!("{}", count);
+}
+
+fn part2(lines: std::io::Lines<BufReader<File>>) {
+    for line in lines {
+        let testValues = line.unwrap().split('|').nth(0).unwrap().trim().split(' ');
+        let realValues = line.unwrap().split('|').nth(1).unwrap().trim().split(' ');
+        let value = 0;
+        println!("{}", testValues.filter(|&x| x.len() == 2).nth(0).unwrap());
+    }
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let lines = io::BufReader::new(file).lines();
+    part1(lines);
+    part2(lines);
 }
