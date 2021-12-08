5d331dc6d73694611727f997ce84d684a749a3f5
Xwilarg <xwilarg@yahoo.fr>
2021-12-08 23:01:03 +0100

    Begin algorithm

--- Day8/main.rs
+++ Day8/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use io::BufReader;
 
-fn part1(lines: std::io::Lines<BufReader<File>>) {
+fn part1() {
+    let file = File::open("input.txt").unwrap();
+    let lines = io::BufReader::new(file).lines();
     let mut count = 0;
     for line in lines {
         for data in line.unwrap().split('|').nth(1).unwrap().trim().split(' ') {
@@ -14,18 +15,21 @@ fn part1(lines: std::io::Lines<BufReader<File>>) {
     println!("{}", count);
 }
 
-fn part2(lines: std::io::Lines<BufReader<File>>) {
+fn part2() {
+    let file = File::open("input.txt").unwrap();
+    let lines = io::BufReader::new(file).lines();
     for line in lines {
-        let testValues = line.unwrap().split('|').nth(0).unwrap().trim().split(' ');
-        let realValues = line.unwrap().split('|').nth(1).unwrap().trim().split(' ');
+        let test_values = line.as_ref().unwrap().split('|').nth(0).unwrap().trim().split(' ');
+        let real_values = line.as_ref().unwrap().split('|').nth(1).unwrap().trim().split(' ');
         let value = 0;
-        println!("{}", testValues.filter(|&x| x.len() == 2).nth(0).unwrap());
+
+        let one = test_values.clone().filter(|&x| x.len() == 2).nth(0).unwrap().chars().collect::<Vec<char>>();
+        let bottom_left = test_values.filter(|&x| x.len() == 6 && x.contains(one[0]) && x.contains(one[1])).nth(0).unwrap();
+        println!("{}", bottom_left);
     }
 }
 
 fn main() {
-    let file = File::open("input.txt").unwrap();
-    let lines = io::BufReader::new(file).lines();
-    part1(lines);
-    part2(lines);
+    part1();
+    part2();
 }
