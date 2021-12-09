76b4d394956662e0b902d4aa5bf1aec4997d7e3f
Xwilarg <xwilarg@yahoo.fr>
2021-12-09 13:01:31 +0100

    Begin part 2 algorithm

--- Day9/main.go
+++ Day9/main.go
@@ -5,8 +5,8 @@ import(
 	"os"
 	"fmt"
 )
-  
-func main() {
+
+func loadData() [][]int {
 	dir, _ := os.Getwd()
 	f, _ := os.Open(dir + "/input.txt")
 	defer f.Close()
@@ -21,6 +21,12 @@ func main() {
 		}
 		data = append(data, [][]int{arr}...)
     }
+
+	return data
+}
+
+func part1() {
+	data := loadData()
 
 	sum := 0
 	for y := 0; y < len(data); y++ {
@@ -37,4 +43,48 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+func fillZone(x, y int, data [][]int) ([][]int, int) {
+	data[y][x] = 9
+	size := 1
+	if x > 0 && data[y][x - 1] != 9 {
+		v := 0
+		data, v = fillZone(x - 1, y, data)
+		size += v
+	}
+	if x < len(data[y]) - 1 && data[y][x + 1] != 9 {
+		v := 0
+		data, v = fillZone(x + 1, y, data)
+		size += v
+	}
+	if y > 0 && data[y - 1][x] != 9 {
+		v := 0
+		data, v = fillZone(x, y - 1, data)
+		size += v
+	}
+	if y < len(data) - 1 && data[y + 1][x] != 9 {
+		v := 0
+		data, v = fillZone(x, y + 1, data)
+		size += v
+	}
+	return data, size
+}
+
+func part2() {
+	data := loadData()
+	size := 0
+	for y := 0; y < len(data); y++ {
+		for x := 0; x < len(data[y]); x++ {
+			if data[y][x] != 9 {
+				data, size = fillZone(x, y, data)
+				fmt.Println(size)
+			}
+		}
+	}
+}
+
+func main() {
+	part1()
+	part2()
 }
