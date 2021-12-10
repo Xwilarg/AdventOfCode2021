97eaebefba7fa6e72bf7247fe1e3fd38c764ab5b
Xwilarg <xwilarg@yahoo.fr>
2021-12-10 11:26:07 +0100

    Add day 10 part 2

--- Day10/main.d
+++ Day10/main.d
@@ -1,6 +1,12 @@
-import std.stdio;
+import std.stdio, std.algorithm;
 
-void main() {
+struct Incomplete
+{
+    string data;
+    string target;
+}
+
+Incomplete[] process() {
     auto file = File("input.txt");
     auto range = file.byLine();
 
@@ -8,6 +14,8 @@ void main() {
     int[] exit = [')', ']', '}', '>'];
     int[] score = [3, 57, 1197, 25_137];
     auto finalScore = 0;
+
+    Incomplete[] incompletes;
 
     foreach (line; range) {
         auto found = false;
@@ -34,6 +42,34 @@ void main() {
                 break;
             }
         }
+        if (!found) {
+            Incomplete i;
+            i.data = line.idup;
+            i.target = target;
+            incompletes ~= i;
+        }
     }
     writeln(finalScore);
+    return incompletes;
+}
+
+void main() {
+    auto incompletes = process();
+    long[] scores;
+    foreach (inc; incompletes) {
+        long score = 0;
+        for (int i = inc.target.length - 1; i >= 0; i--) {
+            score *= 5;
+            final switch (inc.target[i])
+            {
+                case ')': score += 1; break;
+                case ']': score += 2; break;
+                case '}': score += 3; break;
+                case '>': score += 4; break;
+            }
+        }
+        scores ~= score;
+    }
+    scores.sort();
+    writeln(scores[scores.length / 2]);
 }
